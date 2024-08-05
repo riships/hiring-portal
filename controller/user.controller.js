742bb7be5527112d10f27bc8c243642ae19f6006
Rishi <rishi.edunext@gmail.com>
2024-08-05 15:41:24 +0530

    user model

---
+++ controller/user.controller.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(name, email, id, password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+
+    }
+
+    static getUserByMail(email){}
+}
