0a16adc091aca78cb99ba7023dcb15537da41a73
Rishi <rishi.edunext@gmail.com>
2024-08-05 17:22:47 +0530

    implementing view using ejs

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require('express');
 const { router } = require('./router/user.router');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.render('views/index')
+
+})
 
 app.use("/api", router);
+
 
 app.listen(PORT, (err) => {
     if (!err) {
