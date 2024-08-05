e7a75264f4e7ccf467304ef238ee13e07befb7c8
Rishi <rishi.edunext@gmail.com>
2024-08-05 17:26:50 +0530

    running ejs

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const { router } = require('./router/user.router');
 
 const app = express();
+
+
 app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.render('views/index')
-
+    res.render('index')
 })
 
 app.use("/api", router);
