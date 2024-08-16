1911dd91e1929fa953937af49052f60db79871e7
Rishi <rishi.edunext@gmail.com>
2024-08-16 17:09:36 +0530

    bootstrap

--- model/user.model.js
+++ model/user.model.js
@@ -1,8 +1,21 @@
-export const user = class {
+export default User = class {
+    static users = [];
     constructor(name, email, password,) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.createdDate = new Date().toLocaleDateString() + new Date().toLocaleTimeString();
+
     }
+
+    static createUser(name, email, password) {
+        const newUser = new User(name, email, password);
+        User.users.push(newUser);
+        return newUser;
+    }
+
+    static getUserByEmail(email) {
+        return User.users.find(user => user.email === email)
+    }
+
 }
--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get("/", (req, res) => {
     res.render('index')
 })
 
+app.get("/login", (req, res) => {
+    res.render('login')
+})
+
 app.use("/api", router);
 
 
